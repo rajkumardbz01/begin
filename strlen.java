1d935bb7c583b286bef3824b066a988a9f772af9
rajkumardbz01 <rajkumardbz01@gmail.com>
2016-08-13 13:44:49 +0530

    Add files via upload

---
+++ strlen.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class strlen
+{
+public static void main(String []args)
+{
+	Scanner s=new Scanner(System.in);
+	int x=s.nextInt();
+	
+	System.out.println(String.valueOf(x).length());
+}
+}
